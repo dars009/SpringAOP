119cf81bf0752306238d3387bd27ea560169f666
Sagar Navale <Sagar Navale@DESKTOP-GK3S5U1>
2020-02-12 10:18:59 +0530

    First APO Project Share

---
+++ Spring AOP Annotation/src/main/java/com/core/App.java
@@ -0,0 +1,23 @@
+package com.core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.customer.bo.CustomerBo;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("Spring-Customer.xml");
+
+		CustomerBo customer = (CustomerBo) appContext.getBean("customerBo");
+		//customer.addCustomer();
+		
+		//customer.addCustomerReturnValue();
+		
+		//customer.addCustomerThrowException();
+		
+		customer.addCustomerAround("Darshit");
+
+	}
+}
